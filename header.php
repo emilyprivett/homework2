6e70239345e2fe1709a554b6a9accfdcda78fa83
Emily Privett <eaprivett@ou.edu>
2022-09-07 14:16:34 -0500

    attempt

--- header.php
+++ header.php
@@ -24,11 +24,6 @@
   </div>
 </nav>
     <h1>Hello, world!</h1>
-    <form action="handlepost.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-    </form>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 ﻿<?php require_once("header.php"); ?>
-<h1>Index page</h1>
+ <form action="handlepost.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
