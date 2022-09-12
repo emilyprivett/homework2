4d57e4b77cb8e60659bafd2e091f37059d2ffb46
Emily Privett <eaprivett@ou.edu>
2022-09-12 14:03:47 -0500

    function

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 ﻿<?php require_once("header.php"); ?>
 <h1 style="text-align: center;">Welcome to Homework 2!!</h1>
+<?php
+function firstPHPfunction() {
+  echo "This is my first PHP function!";
+}
+
+firstPHPfunction(); ?>
 <?php require_once("footer.php"); ?>
