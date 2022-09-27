4cd1f7fcb4ac4f8fa070f881c117eecc479f6a6f
Emily Privett <eaprivett@ou.edu>
2022-09-26 20:09:20 -0500

    fixed post response

--- handleget.php
+++ handleget.php
@@ -1,6 +1,6 @@
 <?php require_once("header.php"); ?>
 
-    <h1>Post Response</h1>
+    <h1>Get Response</h1>
     Welcome <?php echo $_GET["name"]; ?>! <br>
     Your email address is: <?php echo $_GET["email"]; ?>
 <?php require_once("footer.php"); ?>
--- handlepost.php
+++ handlepost.php
@@ -1,5 +1,5 @@
 <?php require_once("header.php"); ?>
-<h1>Get Response</h1>
-    Your favorite color is<?php echo $_GET["color"]; ?>!! What a great color!<br>
-    You are <?php echo $_GET["age"]; ?> years old.<br>
+<h1>Post Response</h1>
+    Your favorite color is<?php echo $_POST["color"]; ?>!! What a great color!<br>
+    You are <?php echo $_POST["age"]; ?> years old.<br>
 <?php require_once("footer.php"); ?>
