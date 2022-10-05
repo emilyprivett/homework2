9428e83f84fa15b1f4af2957b71ce8ef82f254ba
Emily Privett <eaprivett@ou.edu>
2022-10-05 12:32:32 -0500

    fix

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
 <footer style="position: absolute; bottom: 0; left: 0; right: 0; height:100px; background-color: pink;">
     Homework 2: Emily Privett MIS 4013
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script> 
+</body>
+</html>
